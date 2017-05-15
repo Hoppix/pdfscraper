9d489971461c948891b5e2b235c10b9dcf37c281
Hoppix <k.hopfmann@hotmail.de>
2017-05-15 23:18:23 +0200

    git init

---
+++ scraping.py
@@ -0,0 +1,44 @@
+from urllib.request import urlopen
+from urllib.request import urlretrieve
+import re
+
+
+
+def main():
+    startparse()
+
+
+def startparse():
+    url = input("Input URL to scrape: ")
+    try:
+        html = urlopen(url)
+        print("parsing: " + url)
+        read = html.read().decode('latin-1')
+        parseme(read, url)
+    except:
+        print("Error: No scrapeable URL input.")
+
+def parseme(read, url):
+
+    pattern = re.compile('(<a\shref=\"[^\"]+.pdf\")')
+    links = pattern.findall(read)
+
+    if not links:
+        print("no pdfs found")
+
+    for i in range(len(links)):
+        print("pdf file found: ")
+
+        link = links[i]
+        link = link[9:len(link) -1]
+        name = link.split("/")[-1]
+
+        print("parsing: " +url +  link )
+        try:
+            urlretrieve(url + link, name)
+        except:
+            print("pdf error")
+
+
+if __name__ == "__main__":
+    main()
